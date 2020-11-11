37682c05f6d748c0a0b8b10870f88487ed625923
BriceonWiley <briceonwiley@rice.edu>
2020-11-11 13:38:21 -0600

    server can now show a Normal distribution

--- server.R
+++ server.R
@@ -1,12 +1,20 @@
+# Cmd + Shift + Enter to run App
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
   output$distPlot <- renderPlot({
     # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    mu = as.numeric(input$mu)
+    sig = as.numeric(input$sig)
+    lb = mu - 4 * sig
+    ub = mu + 4 * sig
 
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    ggplot() +
+      stat_function(
+        fun = dnorm, args = list(mean = mu, sd = sig)
+      ) +
+      lims(x = c(lb, ub))
   })
 }
