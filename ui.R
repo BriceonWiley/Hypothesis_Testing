9fee2785af475c5903e4c070737841ccf20e3959
BriceonWiley <briceonwiley@rice.edu>
2020-11-11 13:39:23 -0600

    ui will change based on what type of test we're doing

--- ui.R
+++ ui.R
@@ -1,24 +1,57 @@
+# Cmd + Shift + Enter to run App
+
 library(shiny)
+library(ggplot2)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
+  titlePanel("Hyptothesis Testing"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-        "Number of bins:",
-        min = 1,
-        max = 50,
-        value = 30)
+      selectInput(
+        "test", "Type of Distribution:",
+        choices = c("Normal", "t", "χ^2", "F")
+      ),
+      conditionalPanel(
+        condition = "input.test == 'Normal'" ,
+        textInput(
+          "mu", "Mean", value = 0, width = "20%"
+        ),
+        textInput(
+          "sig", "Standard Deviation", value = 1, width = "20%"
+        )
+      ),
+      conditionalPanel(
+        condition = "input.test == 't'",
+        textInput(
+          "df", "Degrees of Freedom", value = 1, width = "20%"
+        )
+      ),
+      conditionalPanel(
+        condition = "input.test == 'χ^2'",
+        textInput(
+          "df", "Degrees of Freedom", value = 1, width = "20%"
+        )
+      ),
+      conditionalPanel(
+        condition = "input.test == 'F'",
+        textInput(
+          "df1", "First Degrees of Freedom", value = 1, width = "20%"
+        ),
+        textInput(
+          "df2", "Second Degrees of Freedom", value = 1, width = "20%"
+        )
+      )
     ),
+
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      # plotOutput("distPlot")
     )
   )
 )
