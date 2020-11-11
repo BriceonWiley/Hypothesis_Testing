4883a9f30ec3f8fcb39241a6fa3b67cc963b42ff
BriceonWiley <briceonwiley@rice.edu>
2020-11-11 17:35:34 -0600

    server now allows user to swap between type of null distributions

--- server.R
+++ server.R
@@ -4,17 +4,47 @@
 server <- function(input, output) {
 
   output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    mu = as.numeric(input$mu)
-    sig = as.numeric(input$sig)
-    lb = mu - 4 * sig
-    ub = mu + 4 * sig
+    plot_type <- input$test
 
-    # draw the histogram with the specified number of bins
-    ggplot() +
-      stat_function(
-        fun = dnorm, args = list(mean = mu, sd = sig)
-      ) +
-      lims(x = c(lb, ub))
+# Normal Distribution -----------------------------------------------------
+
+    if (plot_type == "Normal") {
+      mu <- as.numeric(input$mu)
+      sigma <- as.numeric(input$sig)
+      lb <- mu - 4 * sigma
+      ub <- mu + 4 * sigma
+      ggplot() +
+        stat_function(fun = dnorm, args = list(mean = mu, sd = sigma)) +
+        lims(x = c(lb, ub))
+
+
+# t Distribution ----------------------------------------------------------
+
+    } else if (plot_type == "t") {
+      df <- as.numeric(input$dft)
+      ggplot() +
+        stat_function(fun = dt, args = list(df = df)) +
+        lims(x = c(-4, 4))
+
+
+# Chi Square Distribution -------------------------------------------------
+
+    } else if (plot_type == "Chi Square") {
+      df <- as.numeric(input$dfc)
+      ggplot() +
+        stat_function(fun = dchisq, args = list(df = df)) +
+        lims(x = c(0, 10))
+
+
+# F Distribution ----------------------------------------------------------
+
+    } else if (plot_type == "F") {
+      df1 <- as.numeric(input$df1)
+      df2 <- as.numeric(input$df2)
+
+      ggplot() +
+        stat_function(fun = df, args = list(df1 = df1, df2 = df2)) +
+        lims(x = c(0, 10))
+    }
   })
 }
